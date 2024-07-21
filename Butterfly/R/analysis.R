71bda6f3148df5c61d49b5914aea58cef53ac15d
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-21 10:10:42 -0400

    Update analysis.R

--- Butterfly/R/analysis.R
+++ Butterfly/R/analysis.R
@@ -42,7 +42,7 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
   total_pairs <- D * (D + 1) / 2
   pb <- progress::progress_bar$new(total = total_pairs, format = "  running [:bar] :percent in :elapsed, eta: :eta", clear = FALSE, width = 60)
   
-  registerDoParallel(cores = detectCores() - 1)
+  parallel::registerDoParallel(cores = detectCores() - 1)
   
   pair_indices <- combn(D, 2, simplify = FALSE)
   pair_indices <- c(pair_indices, lapply(1:D, function(x) c(x, x)))  # Add diagonal pairs
