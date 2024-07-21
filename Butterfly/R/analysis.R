d9f47febd3becff9307ad1ba148d33659f346fb1
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-21 09:54:45 -0400

    Update analysis.R

--- Butterfly/R/analysis.R
+++ Butterfly/R/analysis.R
@@ -5,6 +5,7 @@
 #' @param rhobound Rho bound
 #' @param S Number of simulations
 #' @return A matrix of lists containing confidence intervals and true values for all pairs of taxa
+#' @importFrom progress progress_bar
 run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000, x=NULL) {
   N <- ncol(Y)
   D <- nrow(Y)
