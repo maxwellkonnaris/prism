8a3f33afaee6206025a485d44d547f8259dc5d02
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-21 14:01:26 -0400

    Update analysis.R

--- spider/R/analysis.R
+++ spider/R/analysis.R
@@ -8,11 +8,12 @@
 #' @return A matrix of lists containing confidence intervals and true values for all pairs of taxa
 #' @import progressr
 #' @import foreach
-#' @import doParallel
+#' @import doSNOW
 #' @import parallel
 #' @import MCMCpack
 run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000, num_cores = parallel::detectCores() - 1) {
   library(progressr)
+  library(doSNOW)
   
   N <- ncol(Y)
   D <- nrow(Y)
@@ -45,60 +46,47 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000, n
   }
   
   # Register the parallel backend
-  cl <- parallel::makeCluster(num_cores)
-  doParallel::registerDoParallel(cl)
+  cl <- makeSOCKcluster(num_cores)
+  registerDoSNOW(cl)
   
   on.exit({
-    parallel::stopCluster(cl)
-    doParallel::stopImplicitCluster()
+    stopCluster(cl)
   }, add = TRUE)
-  
-  # Verify cluster registration
-  if (!foreach::getDoParRegistered()) {
-    stop("Parallel backend is not registered.")
-  } else {
-    cat("Parallel backend is registered.\n")
-  }
-  
-  # Check the number of workers
-  num_workers <- foreach::getDoParWorkers()
-  cat("Number of workers: ", num_workers, "\n")
   
   start_time <- Sys.time()
   
   total_pairs <- D * (D + 1) / 2
   pair_indices <- combn(D, 2, simplify = FALSE)
   pair_indices <- c(pair_indices, lapply(1:D, function(x) c(x, x)))  # Add diagonal pairs
-                                         
-  p <- progressr::progressor(along = 1:total_pairs)
-  
-  results_list <- with_progress({
-    foreach::foreach(pair = pair_indices, .combine = 'c', .packages = c('stats', 'progressr', 'MCMCpack')) %dopar% {
-      d1 <- pair[1]
-      d2 <- pair[2]
-      
-      minmaxsigma <- matrix(NA, S, 2)
-      for (s in 1:S) {
-        Yboot <- Y[, sample(1:N, replace = TRUE)]
-        
-        res <- optim(par = c(0, 0, 0.1), fn = objective_function, Yboot = Yboot, d1 = d1, d2 = d2, alpha = alpha, method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, 0.2))
-        
-        minmaxsigma[s, ] <- c(min(res$value), max(res$value))
-      }
-      
-      sortedmin <- sort(minmaxsigma[, 1])
-      sortedmax <- sort(minmaxsigma[, 2])
-      
-      cilower <- quantile(sortedmin, probs = 0.025)
-      ciupper <- quantile(sortedmax, probs = 0.975)
 
-      finitesamplecovariance <- cov(Y[d1,], Y[d2,])
+  pb <- txtProgressBar(min = 1, max = length(pair_indices), style = 3)
+  progress <- function(n) setTxtProgressBar(pb, n)
+  opts <- list(progress = progress)
+  
+  results_list <- foreach::foreach(pair = pair_indices, .combine = 'c', .packages = c('stats', 'MCMCpack'), .options.snow = opts) %dopar% {
+    d1 <- pair[1]
+    d2 <- pair[2]
+    
+    minmaxsigma <- matrix(NA, S, 2)
+    for (s in 1:S) {
+      Yboot <- Y[, sample(1:N, replace = TRUE)]
       
-      p(message = sprintf("Processing pair %d-%d", d1, d2))
+      res <- optim(par = c(0, 0, 0.1), fn = objective_function, Yboot = Yboot, d1 = d1, d2 = d2, alpha = alpha, method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, 0.2))
       
-      list(cilower = cilower, ciupper = ciupper, finitesamplecovariance = finitesamplecovariance)
+      minmaxsigma[s, ] <- c(min(res$value), max(res$value))
     }
-  })
+    
+    sortedmin <- sort(minmaxsigma[, 1])
+    sortedmax <- sort(minmaxsigma[, 2])
+    
+    cilower <- quantile(sortedmin, probs = 0.025)
+    ciupper <- quantile(sortedmax, probs = 0.975)
+    
+    finitesamplecovariance <- cov(Y[d1,], Y[d2,])
+    
+    list(cilower = cilower, ciupper = ciupper, finitesamplecovariance = finitesamplecovariance)
+  }
+  close(pb)
   
   for (i in 1:length(pair_indices)) {
     pair <- pair_indices[[i]]
