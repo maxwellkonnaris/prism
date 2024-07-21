d4d36a7b133da6c2c89bae80c431e8d78cc9c4d4
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-21 10:03:50 -0400

    Update analysis.R

--- Butterfly/R/analysis.R
+++ Butterfly/R/analysis.R
@@ -5,8 +5,8 @@
 #' @param rhobound Rho bound
 #' @param S Number of simulations
 #' @return A matrix of lists containing confidence intervals and true values for all pairs of taxa
-#' @importFrom progress progress_bar
-run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000, x=NULL) {
+#' @importFrom progress progress_bar doParallel foreach
+run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
   N <- ncol(Y)
   D <- nrow(Y)
   
@@ -29,9 +29,9 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000, x
     }
     rWpara <- log(rWpara)
     
-    a <- var(rWpara[d1,])
-    b <- var(rWpara[d2,])
-    c <- cor(rWpara[d1,], rWpara[d2,])
+    a <- var(rWpara[d1, ])
+    b <- var(rWpara[d2, ])
+    c <- cor(rWpara[d1, ], rWpara[d2, ])
     
     sigma <- a * b * c + a * x * rho1 + b * x * rho2 + x^2
     return(min(sigma))
@@ -42,30 +42,40 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000, x
   total_pairs <- D * (D + 1) / 2
   pb <- progress::progress_bar$new(total = total_pairs, format = "  running [:bar] :percent in :elapsed, eta: :eta", clear = FALSE, width = 60)
   
-  counter <- 0
+  registerDoParallel(cores = detectCores() - 1)
   
-  for (d1 in 1:D) {
-    for (d2 in d1:D) {
-      minmaxsigma <- matrix(NA, S, 2)
-      for (s in 1:S) {
-        Yboot <- Y[, sample(1:N, replace = TRUE)]
-        
-        res <- optim(par = c(0, 0, 0.1), fn = objective_function, Yboot = Yboot, d1 = d1, d2 = d2, alpha = alpha, method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, 0.2))
-        
-        minmaxsigma[s, ] <- c(res$value, res$value)
-      }
+  pair_indices <- combn(D, 2, simplify = FALSE)
+  pair_indices <- c(pair_indices, lapply(1:D, function(x) c(x, x)))  # Add diagonal pairs
+  
+  results_list <- foreach(pair = pair_indices, .combine = 'c', .packages = c('stats', 'progress')) %dopar% {
+    d1 <- pair[1]
+    d2 <- pair[2]
+    
+    minmaxsigma <- matrix(NA, S, 2)
+    for (s in 1:S) {
+      Yboot <- Y[, sample(1:N, replace = TRUE)]
       
-      sortedmin <- sort(minmaxsigma[, 1])
-      sortedmax <- sort(minmaxsigma[, 2])
+      res <- optim(par = c(0, 0, 0.1), fn = objective_function, Yboot = Yboot, d1 = d1, d2 = d2, alpha = alpha, method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, 0.2))
       
-      cilower <- quantile(sortedmin, probs = 0.025)
-      ciupper <- quantile(sortedmax, probs = 0.975)
-      
-      results[[d1, d2]] <- list(cilower = cilower, ciupper = ciupper, minmaxsigma = minmaxsigma)
-      
-      counter <- counter + 1
-      pb$tick()
+      minmaxsigma[s, ] <- c(res$value, res$value)
     }
+    
+    sortedmin <- sort(minmaxsigma[, 1])
+    sortedmax <- sort(minmaxsigma[, 2])
+    
+    cilower <- quantile(sortedmin, probs = 0.025)
+    ciupper <- quantile(sortedmax, probs = 0.975)
+    
+    pb$tick()
+    
+    list(cilower = cilower, ciupper = ciupper, minmaxsigma = minmaxsigma)
+  }
+  
+  for (i in 1:length(pair_indices)) {
+    pair <- pair_indices[[i]]
+    d1 <- pair[1]
+    d2 <- pair[2]
+    results[[d1, d2]] <- results_list[[i]]
   }
   
   end_time <- Sys.time()
