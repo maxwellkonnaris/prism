8c800ddc12143fffcb9ace88103856d30cfef8be
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-21 10:39:18 -0400

    Update analysis.R

--- Butterfly/R/analysis.R
+++ Butterfly/R/analysis.R
@@ -72,7 +72,7 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
   pair_indices <- combn(D, 2, simplify = FALSE)
   pair_indices <- c(pair_indices, lapply(1:D, function(x) c(x, x)))  # Add diagonal pairs
   
-  results_list <- foreach::foreach(pair = pair_indices, .combine = 'c', .packages = c('stats', 'progress')) %dopar% {
+  results_list <- foreach::foreach(pair = pair_indices, .combine = 'c', .packages = c('stats', 'progress', 'MCMCpack')) %dopar% {
     d1 <- pair[1]
     d2 <- pair[2]
     
@@ -82,7 +82,7 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
       
       res <- optim(par = c(0, 0, 0.1), fn = objective_function, Yboot = Yboot, d1 = d1, d2 = d2, alpha = alpha, method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, 0.2))
       
-      minmaxsigma[s, ] <- c(res$value, res.value)
+      minmaxsigma[s, ] <- c(res$value, res$value)
     }
     
     sortedmin <- sort(minmaxsigma[, 1])
