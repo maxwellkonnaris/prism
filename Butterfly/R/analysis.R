0b7c3c4455df43935cae39570d93d14a4d5308cb
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-21 10:30:03 -0400

    Update analysis.R

--- Butterfly/R/analysis.R
+++ Butterfly/R/analysis.R
@@ -21,7 +21,11 @@ cat("Number of workers: ", num_workers, "\n")
 #' @param rhobound Rho bound
 #' @param S Number of simulations
 #' @return A matrix of lists containing confidence intervals and true values for all pairs of taxa
-#' @importFrom progress progress_bar doParallel foreach parallel
+#' @import progress 
+#' @import foreach
+#' @import doParallel
+#' @import parallel
+#' @import progress_bar
 run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
   N <- ncol(Y)
   D <- nrow(Y)
