5ea73189fdde088cf98ec96c52a0ba2c115c5f06
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-22 13:20:49 -0400

    Update simulation.R

--- spider/R/simulation.R
+++ spider/R/simulation.R
@@ -1,9 +1,15 @@
 #' Simulate Data
 #'
-#' @param D Number of dimensions
-#' @param N Number of samples
-#' @param seq.depth Sequencing depth
-#' @return Simulated data matrix
+#' This function simulates data for given dimensions and sample size, with specified sequencing depth.
+#'
+#' @param D An integer specifying the number of dimensions.
+#' @param N An integer specifying the number of samples.
+#' @param seq.depth A numeric value specifying the sequencing depth.
+#' @return A list containing the simulated data matrix \code{Y}, the log-transformed matrix \code{logW}, and the covariance matrix \code{Sigma}.
+#' @examples
+#' # Example usage:
+#' simulated_data <- simulate_data(3, 100, 1000)
+#' @export
 simulate_data <- function(D, N, seq.depth) {
   Sigma <- diag(D)
   Sigma[2, 1] <- Sigma[1, 2] <- -0.5
@@ -16,4 +22,3 @@ simulate_data <- function(D, N, seq.depth) {
 
   return(list(Y = Y, logW = logW, Sigma = Sigma))
 }
-
