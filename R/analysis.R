8b0fc18d27d02421facc28a182af2ec37651c6e3
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-23 10:26:05 -0400

    Update analysis.R

--- R/analysis.R
+++ R/analysis.R
@@ -21,7 +21,7 @@
 #' results <- run_analysis(Y)
 #' @export
 
-run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
+run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 1.0, S = 1000) {
 
   # Record the start time for profiling
   start_time <- Sys.time()
@@ -184,8 +184,8 @@ run_analysis <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 0.8, S = 1000) {
         -objective_function(params, Yboot, d1, d2, alpha)
       }, Yboot = Yboot, d1 = d1, d2 = d2, alpha = alpha, method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, 1.0))
             
-      sigma_values <<- rbind(sigma_values, data.frame(d1 = d1, d2 = d2, rho1 = res_min$par[1], rho2 = res_min$par[2], x = res_min$par[3], sigma = res_min$value, stringsAsFactors = FALSE))
-      sigma_values <<- rbind(sigma_values, data.frame(d1 = d1, d2 = d2, rho1 = res_max$par[1], rho2 = res_max$par[2], x = res_max$par[3], sigma = -res_max$value, stringsAsFactors = FALSE))
+      sigma_values <<- rbind(sigma_values, data.frame(Yboot = Yboot, d1 = d1, d2 = d2, rho1 = res_min$par[1], rho2 = res_min$par[2], x = res_min$par[3], sigma = res_min$value, stringsAsFactors = FALSE))
+      sigma_values <<- rbind(sigma_values, data.frame(Yboot = Yboot, d1 = d1, d2 = d2, rho1 = res_max$par[1], rho2 = res_max$par[2], x = res_max$par[3], sigma = -res_max$value, stringsAsFactors = FALSE))
       
       c(minsigma = res_min$value, maxsigma = -res_max$value)
     }
