6549c249869098ad8a142ebbe83f3100f24f5182
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-24 17:29:42 -0400

    Update sigmaplots.R

--- R/sigmaplots.R
+++ R/sigmaplots.R
@@ -16,9 +16,9 @@
 #' # results <- run_analysis(Y)
 #' # all_inner_results <- results$all_inner_results
 #' # sigmaplot(all_inner_results)  # Without saving
-#' # sigmaplot(all_inner_results, save_format = "png", plot_type = "line")  # Save as PNG with best fit lines
+#' # sigmaplot(all_inner_results, save = "png", plot_type = "line")  # Save as PNG with best fit lines
 #' @export
-sigmaplot <- function(all_inner_results, filename = NULL, save_format = NULL, plot_type = "line") {
+sigmaplot <- function(all_inner_results, filename = NULL, save = NULL, plot_type = "line") {
   
   # Convert character columns to numeric
   all_inner_results$s <- as.numeric(all_inner_results$s)
@@ -86,12 +86,12 @@ sigmaplot <- function(all_inner_results, filename = NULL, save_format = NULL, pl
   
   # Save plots to files if save_format is specified
   if (!is.null(save_format)) {
-    ggsave(filename = paste0(filename,"Min_Sigma_vs_rho1_", plot_type, ".", save_format), plot = p1_min, width = 10, height = 10, dpi = 300)
-    ggsave(filename = paste0(filename,"Max_Sigma_vs_rho1_", plot_type, ".", save_format), plot = p1_max, width = 10, height = 10, dpi = 300)
-    ggsave(filename = paste0(filename,"Min_Sigma_vs_rho2_", plot_type, ".", save_format), plot = p2_min, width = 10, height = 10, dpi = 300)
-    ggsave(filename = paste0(filename,"Max_Sigma_vs_rho2_", plot_type, ".", save_format), plot = p2_max, width = 10, height = 10, dpi = 300)
-    ggsave(filename = paste0(filename,"Min_Sigma_vs_x_", plot_type, ".", save_format), plot = p3_min, width = 10, height = 10, dpi = 300)
-    ggsave(filename = paste0(filename,"Max_Sigma_vs_x_", plot_type, ".", save_format), plot = p3_max, width = 10, height = 10, dpi = 300)
-    #ggsave(filename = paste0(filename, "Combined_", plot_type, ".", save_format), plot = combined_plot, width = 15, height = 30, dpi = 300)
+    ggsave(filename = paste0(filename,"Min_Sigma_vs_rho1_", plot_type, ".", save), plot = p1_min, width = 10, height = 10, dpi = 300)
+    ggsave(filename = paste0(filename,"Max_Sigma_vs_rho1_", plot_type, ".", save), plot = p1_max, width = 10, height = 10, dpi = 300)
+    ggsave(filename = paste0(filename,"Min_Sigma_vs_rho2_", plot_type, ".", save), plot = p2_min, width = 10, height = 10, dpi = 300)
+    ggsave(filename = paste0(filename,"Max_Sigma_vs_rho2_", plot_type, ".", save), plot = p2_max, width = 10, height = 10, dpi = 300)
+    ggsave(filename = paste0(filename,"Min_Sigma_vs_x_", plot_type, ".", save), plot = p3_min, width = 10, height = 10, dpi = 300)
+    ggsave(filename = paste0(filename,"Max_Sigma_vs_x_", plot_type, ".", save), plot = p3_max, width = 10, height = 10, dpi = 300)
+    #ggsave(filename = paste0(filename, "Combined_", plot_type, ".", save), plot = combined_plot, width = 15, height = 30, dpi = 300)
   }
 }
