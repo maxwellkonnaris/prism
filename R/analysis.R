8302877adada42e842d142d9c49e6b09ec1a8c2e
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-07-29 12:36:09 -0400

    Update analysis.R

--- R/analysis.R
+++ R/analysis.R
@@ -178,6 +178,7 @@ estimate_covariance <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 1.0, S = 
       
       # Use parallel foreach for the inner loop
       results_inner <- foreach(s = 1:S, .combine = 'rbind', .packages = c('stats', 'MCMCpack'), .options.snow = opts) %dopar% {
+        tryCatch({
           Yboot <- Y[, bootstrap_samples[[s]]]
           
           rWpara <- matrix(NA, D, N)
@@ -191,15 +192,11 @@ estimate_covariance <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 1.0, S = 
           c <- cor(rWpara[d1, ], rWpara[d2, ])
           
           # Find the minimum sigma
-          res_min <- optim(par = c(0, 0, 0.5), fn = function(params, a, b, c) {
-          	objective_function(par, a, b, c) 
-          	}, a = a, b = b, c = c,
+          res_min <- optim(params = c(0, 0, 0.5), a = a, b = b, c = c, fn = objective_function(params, a, b, c),
         	method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, upperx))
           
           # Find the maximum sigma by negating the objective function
-          res_max <- optim(par = c(0, 0, 0.5), fn =  function(params, a, b, c) {
-          	-objective_function(par, a, b, c)
-          	}, a = a, b = b, c = c,
+          res_max <- optim(params = c(0, 0, 0.5), a = a, b = b, c = c, fn = function(params, a, b, c) {-objective_function(params, a, b, c)}, 
           		   method = "L-BFGS-B", lower = c(-rhobound, -rhobound, 0.05), upper = c(rhobound, rhobound, upperx))
           
           data.frame(
@@ -218,8 +215,12 @@ estimate_covariance <- function(Y, alpha = rep(0, nrow(Y)), rhobound = 1.0, S = 
             b = b,
             c = c,
           )
+      }, error = function(e) {
+        message("Error in inner loop: ", e$message)
+        return(NULL)
+        })
       }
-  
+      
       # Gather the min and max optimized sigmas
       minsigma_values <- results_inner$minsigma
       maxsigma_values <- results_inner$maxsigma
