c1a6f07903a07690df04ac8c8e241c3983c3ce78
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-08-20 15:27:03 -0400

    Update estimation.R

--- R/estimation.R
+++ R/estimation.R
@@ -196,10 +196,10 @@ estimate_covariance <- function(Y, alpha = 0.5, lowerrhobound = -1.0, upperrhobo
           relativecovariance <- cov(rWpara[1, ], rWpara[2, ])
           
           # Find the minimum sigma
-          res_min <- optim(par = c(0, 0, 0.5), taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = objective_function, method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev))
+          res_min <- optim(par = c(((upperrhobound-lowerrhobound)/2), ((upperrhobound-lowerrhobound)/2), (upperscalestdev - lowerscalestdev)/2)), taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = objective_function, method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev))
           
           # Find the maximum sigma by negating the objective function
-          res_max <- optim(par = c(0, 0, 0.5), taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = function(params, taxa1relativesd, taxa2relativesd, relativecorrelation) {-objective_function(params, taxa1relativesd, taxa2relativesd, relativecorrelation)}, method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev))
+          res_max <- optim(par = c(((upperrhobound-lowerrhobound)/2), ((upperrhobound-lowerrhobound)/2), (upperscalestdev - lowerscalestdev)/2)), taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = function(params, taxa1relativesd, taxa2relativesd, relativecorrelation) {-objective_function(params, taxa1relativesd, taxa2relativesd, relativecorrelation)}, method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev))
           
           data.frame(
             d1 = d1,
