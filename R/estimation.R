adb7d310185627367efd2c330b9c9aa93824494a
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-08-20 18:20:09 -0400

    Update estimation.R

--- R/estimation.R
+++ R/estimation.R
@@ -213,10 +213,10 @@ estimate_covariance <- function(Y, alpha = 0.5, lowerrhobound = -1.0, upperrhobo
           relativecovariance <- cov(rWpara[1, ], rWpara[2, ])
           
           # Find the minimum sigma
-          res_min <- optim(par = initialparameters, taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = objective_function, gr = function(params) gradient_function(params, taxa1relativesd, taxa2relativesd, relativecorrelation), method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev))
+          res_min <- optim(par = initialparameters, taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = objective_function, gr = gradient_function, method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev))
           
           # Find the maximum sigma by negating the objective function
-          res_max <- optim(par = initialparameters, taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = objective_function, gr = function(params) gradient_function(params, taxa1relativesd, taxa2relativesd, relativecorrelation), method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev), control = list(fnscale = -1))
+          res_max <- optim(par = initialparameters, taxa1relativesd = taxa1relativesd, taxa2relativesd = taxa2relativesd, relativecorrelation = relativecorrelation, fn = objective_function, gr = gradient_function, method = "L-BFGS-B", lower = c(lowerrhobound, lowerrhobound, lowerscalestdev), upper = c(upperrhobound, upperrhobound, upperscalestdev), control = list(fnscale = -1))
           
           data.frame(
             d1 = d1,
