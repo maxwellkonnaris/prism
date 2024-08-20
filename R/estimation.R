16959138eaedfb7452d023263322bcc44387bee3
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-08-20 17:07:26 -0400

    Update estimation.R

--- R/estimation.R
+++ R/estimation.R
@@ -99,10 +99,10 @@ estimate_covariance <- function(Y, alpha = 0.5, lowerrhobound = -1.0, upperrhobo
     
     sigma <- taxa1relativesd * taxa2relativesd * relativecorrelation + taxa1relativesd * scalestdev * taxa1scalecorrelation + taxa2relativesd * scalestdev * taxa2scalecorrelation + scalestdev^2
 
-    # Check if the matrix is SPSD
-    if (!checkSPSD(sigma)) {
-      return(1e10)
-    }
+    # # Check if the matrix is SPSD
+    # if (!checkSPSD(sigma)) {
+    #   return(1e10)
+    # }
 
     return(sigma)
   }
