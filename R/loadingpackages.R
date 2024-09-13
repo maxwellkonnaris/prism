f7f4d01f009e248911b49589e2a545970713479d
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-13 13:54:28 -0400

    Create loadingpackages.R

---
+++ R/loadingpackages.R
@@ -0,0 +1,29 @@
+#' @title Package Startup Script
+#' @description Installs necessary GitHub and Bioconductor packages
+#' @details
+#' This function installs the following packages if they are not already installed:
+#' - GitHub packages: driver, propr, SpiecEasi, banocc
+#' - Bioconductor package: phyloseq
+#' The function is executed upon loading the package.
+#' @param libname The library name.
+#' @param pkgname The package name.
+#' @importFrom devtools install_github
+#' @importFrom BiocManager install
+.onLoad <- function(libname, pkgname) {
+  if (!requireNamespace("driver", quietly = TRUE)) {
+    devtools::install_github("jsilve24/driver")
+  }
+  if (!requireNamespace("propr", quietly = TRUE)) {
+    devtools::install_github("tpq/propr")
+  }
+  if (!requireNamespace("SpiecEasi", quietly = TRUE)) {
+    devtools::install_github("zdk123/SpiecEasi")
+  }
+  if (!requireNamespace("banocc", quietly = TRUE)) {
+    devtools::install_github("biobakery/banocc")
+  }
+  if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+  }
+  BiocManager::install("phyloseq")
+}
