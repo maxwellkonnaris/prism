6e4795786e2c180a83cafbe6f6f50462effd534c
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-13 09:45:50 -0400

    Update plotting_functions.R

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1096,8 +1096,8 @@ plot_bivariate_grid <- function(data, output_directory = "plots/", sample_size =
 #' @examples
 #' # Assuming you have a dataframe called `results_df` with columns for comparison and proportion_intervals_dontcoverzero
 #' # proportiondontoverzerobars(results_df)
-proportiondontoverzerobars <- function(data, comparison_col = "comparison", proportion_col = "proportion_intervals_dontcoverzero", ci_upper_col = "ninetyfive_ci_upper", 
-                                            ci_lower_col = "ninetyfive_ci_lower") {
+proportiondontoverzerobars <- function(data, comparison_col = "comparison", proportion_col = "proportion_intervals_dontcoverzero", ci_upper = "ninetyfive_ci_upper", 
+                                            ci_lower = "ninetyfive_ci_lower") {
   
   # Create a new column to flag whether the 95% CI does not cover zero
   data$does_not_cover_zero <- ifelse(
