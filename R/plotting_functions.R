7b3ad0b825bc6dd9a7a7660ba50d483a421a6923
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-13 00:03:40 -0400

    Update plotting_functions.R

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1098,8 +1098,7 @@ plot_bivariate_grid <- function(data, output_directory = "plots/", sample_size =
 #' # proportiondontoverzerobars(results_df)
 proportiondontoverzerobars <- function(data, comparison_col="comparison", proportion_col="proportion_intervals_dontcoverzero") {
   
-  # Sort the dataframe by proportion_col in descending order
-  data <- data[order(-data[[proportion_col]]), ]
+  data <- data[order(data$proportion_col, decreasing = TRUE), ]
   
   # Plot the bar plot using ggplot2
   ggplot(data, aes_string(x = comparison_col, y = proportion_col)) +
