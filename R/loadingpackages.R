a3cff0bafb11ed0196a011add6eab5a465fa83e0
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-19 23:49:27 -0400

    Update loadingpackages.R

--- R/loadingpackages.R
+++ R/loadingpackages.R
@@ -1,31 +1,37 @@
 #' @title Package Startup Script
-#' @description Installs necessary GitHub and Bioconductor packages
-#' @details
-#' This function installs the following packages if they are not already installed:
-#' - GitHub packages: driver, propr, SpiecEasi
-#' - Bioconductor package: phyloseq, banocc
-#' The function is executed upon loading the package.
+#' @description Installs necessary GitHub and Bioconductor packages if not installed.
 #' @param libname The library name.
 #' @param pkgname The package name.
-#' @importFrom devtools install_github
-#' @importFrom BiocManager install
 .onLoad <- function(libname, pkgname) {
+  # Check and install BiocManager if not available
   if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
-  } 
+  }
+  
+  # Install GitHub packages if not installed
   if (!requireNamespace("driver", quietly = TRUE)) {
+    message("Installing 'driver' package from GitHub...")
     devtools::install_github("jsilve24/driver")
   }
+  
   if (!requireNamespace("propr", quietly = TRUE)) {
+    message("Installing 'propr' package from GitHub...")
     devtools::install_github("tpq/propr")
   }
+  
   if (!requireNamespace("SpiecEasi", quietly = TRUE)) {
+    message("Installing 'SpiecEasi' package from GitHub...")
     devtools::install_github("zdk123/SpiecEasi")
   }
+  
+  # Install Bioconductor packages if not installed
   if (!requireNamespace("banocc", quietly = TRUE)) {
-    BiocManager::install("banocc", force=TRUE)
+    message("Installing 'banocc' package from Bioconductor...")
+    BiocManager::install("banocc")
   }
+  
   if (!requireNamespace("phyloseq", quietly = TRUE)) {
-    BiocManager::install("phyloseq", force=TRUE)
+    message("Installing 'phyloseq' package from Bioconductor...")
+    BiocManager::install("phyloseq")
   }
 }
