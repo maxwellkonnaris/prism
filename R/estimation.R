82a9397c288d7c140cef020344b397fdd7e051d1
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-24 17:00:23 -0400

    Update estimation.R

--- R/estimation.R
+++ R/estimation.R
@@ -235,10 +235,14 @@ estimate_covariance <- function(Y, alpha = 0.5, uncertaintydistribution = "Multi
   ## END PROGRESS BARS -------------------------------------------------------------------------------------------------------------------------------------
   ## ACCOUNTING FOR UNCERTAINTY IN FINITE SAMPLING ---------------------------------------------------------------------------------------------------------
   ## calculate bootstrap resampling -- accounting for finite sampling
+  # boostrap_samples <- matrix(NA, N, S)
+  # for (s in 1:S) {
+  #   boostrap_samples[,s] <- sample(1:N, replace=TRUE)
+  # }
   boostrap_samples <- matrix(NA, N, S)
-  for (s in 1:S) {
-    boostrap_samples[,s] <- sample(1:N, replace=TRUE)
-  }
+    for (s in 1:S) {
+      boostrap_samples[,s] <- 1:N
+    }
   ## END ACCOUNTING FOR UNCERTAINTY IN FINITE SAMPLING -----------------------------------------------------------------------------------------------------
   ## ACCOUNTING FOR UNCERTAINTY IN OBSERVED RELATIVE ABUNDANCES --------------------------------------------------------------------------------------------
   # Dimensions: (n_taxa, n_samples, n_iter) -- populate matrix of NAs
