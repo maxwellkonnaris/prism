a817ac1d96d4c9918b4de947580357d99cedf8ee
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-29 22:22:03 -0400

    Update helper_functions.R

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -857,9 +857,9 @@ prism.simulate_data_sparsecorr <- function(
   frequent_taxa_indices <- (n_rare + n_medium + 1):n_taxa
   
   ## 2. Assign Means Based on Categories
-  means_rare <- runif(n_rare, .001, .05)
-  means_medium <- runif(n_medium, .06, .4)
-  means_frequent <- runif(n_frequent, .5, .9)
+  means_rare <- runif(n_rare, .0001, .005)
+  means_medium <- runif(n_medium, .006, .07)
+  means_frequent <- runif(n_frequent, .5, .7)
 
   means_rare <- means_rare * total_abundance_scale
   means_medium <- means_medium * total_abundance_scale
