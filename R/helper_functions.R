566674a52471daae654b89407dc5303a836718b7
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-09-30 00:38:40 -0400

    Update helper_functions.R

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1029,7 +1029,8 @@ prism.simulate_data_sparsecorr <- function(
     taxa_means_pre = taxa_means_pre,
     taxa_means_post = taxa_means_post,
     flow = flow_data,
-    corr_matrix = corr_matrix
+    corr_matrix = corr_matrix, 
+    cov_matrix - log_cov_matrix
   )
   
   if (replicates > 1) {
