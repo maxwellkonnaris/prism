059199a2ffdfbe5da427f8d63188bd864df4c507
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2024-10-03 22:38:20 -0400

    Update helper_functions.R

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -845,14 +845,16 @@ prism.simulate_prepost <- function(
       ungroup()
   }
   
-  # Convert W_combined to a data frame and assign column names
+  # Format dataframes
   colnames(W) <- paste0("Taxa", 1:ncol(W)) 
   dummy <- as.data.frame(W)
   colnames(dummy) <- paste0("Taxa", 1:ncol(W)) 
   dummy$Condition <- Condition
+  colnames(Y) <- paste0("Taxa", 1:ncol(Y)) 
   
-  names(taxa_means_pre) <- paste0("Taxa_", 1:n_taxa)
-  
+  names(taxa_means_pre) <- paste0("Taxa", 1:n_taxa)
+  names(taxa_means_post) <- paste0("Taxa", 1:n_taxa)
+            
   ## 13. Compile Results
   results <- list(
     W.pre = W_pre,
